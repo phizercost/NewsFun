b43a4e7c71f2c9d7dbbca447b1bd30efae2ed182
phizercost <phizer.development@gmail.com>
2019-01-22 17:41:30 +0200

    Add JSON Parsing from the Alamofire results

--- NewsFun/NewsHelper.swift
+++ NewsFun/NewsHelper.swift
@@ -15,7 +15,34 @@ class NewsHelper {
     func  getArticles() {
         Alamofire.request("https://newsapi.org/v2/top-headlines?country=us&apiKey=35d7fae559864651b2a40da1db343491").responseJSON { (response) in
             print(response)
+            
+            if let json = response.result.value as? [String: Any] {
+                if let jsonArticles = json["articles"] as? [[String:Any]] {
+                    var articles  = [Article]()
+                    for jsonArticle in jsonArticles {
+                        guard let title = jsonArticle["title"] as? String , let urlToImage = jsonArticle["urlToImage"] as? String , let url = jsonArticle["url"] as? String , let description = jsonArticle["description"] as? String
+                        else {
+                              continue
+                        }
+                        let article = Article()
+                        article.title = title
+                        article.url = url
+                        article.urlToImage = urlToImage
+                        article.description = description
+                        articles.append(article)
+                    }
+                }
+            }
         }
     }
     
+}
+
+
+class Article {
+    var title = ""
+    var urlToImage = ""
+    var url = ""
+    var description = ""
+    var category = ""
 }
